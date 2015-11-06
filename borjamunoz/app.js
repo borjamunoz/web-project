88613ee2e1240f3b0f597a700b58d713e4963728
Borja <munoz.borja@gmail.com>
2015-11-06 16:14:07 +0000

    cambios en form

--- borjamunoz/app.js
+++ borjamunoz/app.js
@@ -10,6 +10,10 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var contact = require('./routes/contact');
+//module to manage mails with gmail
+var nodemailer = require('nodemailer');
+
 var app = express();
 
 // view engine setup
@@ -26,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.get('/contact', routes);
+app.get('/contact', contact);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -59,6 +63,34 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.post('/contact', function (req, res) {
+  var mailOpts, smtpTrans;
+  //Setup Nodemailer transport, I chose gmail. Create an application-specific password to avoid problems.
+  smtpTrans = nodemailer.createTransport('SMTP', {
+      service: 'Gmail',
+      auth: {
+          user: "munoz.borja@gmail.com"//,
+          //pass: "application-specific-password" 
+      }
+  });
+  //Mail options
+  mailOpts = {
+      from: req.body.name + ' &lt;' + req.body.email + '&gt;', //grab form data from the request body object
+      to: 'munoz.borja@gmail.com',
+      subject: 'Website contact form',
+      text: req.body.message
+  };
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+      //Email not sent
+      if (error) {
+          res.render('contact', { title: 'Contact', msg: 'Error occured, message not sent.', err: true, page: 'contact' });
+      }
+      //Yay!! Email sent
+      else {
+          res.render('contact', { title: 'Contact', msg: 'Message sent! Thank you.', err: false, page: 'contact' });
+      }
+  });
+});
 
 //Server
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080 || 3000, ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
